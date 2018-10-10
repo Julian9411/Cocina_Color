a4ba3fdd028e55d3d4740f6b7a732196d8eba7f2
julian <julian@mundobakia.com>
2018-10-10 13:52:23 -0500

    añade archivo scrips.js

--- js/scripts.js
+++ js/scripts.js
@@ -25,14 +25,14 @@
     suscribite.addEventListener('click', registro);
 
     //Funciones
-
+/*
     function registro(event){
         event.preventDefault();
         if (suscribite){
             this.style.display = 'none';
             datosUsuario.style.display='block';
         }
-    };
+    };*/
 
     function validarRegistro(event){
         event.preventDefault();
@@ -50,5 +50,43 @@
             alert('Este campo de llevar una @');
         }
     }
+    console.log('listo')
 
 })();
+
+$(function (){
+
+//se agregan atributos al menu de redes dociales
+
+    $('div.contenido_footer .nosotros h2').text('Incentivate a realizar un cambio');
+    $('div.contenido_footer .nosotros p').text('Te alimentaras adecuadamente con estas ideas, daras más oportunidad de vida, y haras para ti una nueva oportunidad')
+
+    $('.menu_redes_sociales ul li a:first').attr('href', 'https://www.facebook.com');
+    $('.menu_redes_sociales ul li:first').next().children().attr('href', 'https://www.instagram.com/?hl=es-la');
+    $('.menu_redes_sociales ul li:first').next().next().children().attr('href', 'https://www.youtube.com');
+    $('.sociales .menu_redes_sociales ul li:first').children().attr('href', 'https://www.facebook.com');
+    $('.sociales .menu_redes_sociales ul li:first').next().children().attr('href', 'https://www.instagram.com/?hl=es-la');
+    $('.sociales .menu_redes_sociales ul li:first').next().next().children().attr('href', 'https://www.youtube.com');
+
+//newslatter
+    $('div.newslater a').on('click', function(){
+        $(this).hide();
+        $('#datos_usuario').show(1000);
+    })
+
+//menu_principal
+    $('nav.navegacion_principal ul li').on('click', function(){
+        $('nav.navegacion_principal ul li').removeClass('activo')
+       $(this).addClass('activo')
+    });
+
+//menu_movil
+    $('.menu_movil').on('click', function(){
+
+        $('.navegacion_principal ul').slideDown(1000);
+    })
+    
+    console.log('correcto')
+
+
+})
