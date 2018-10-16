5520aa51f654833f70f194609ef839a7fd4f457d
julian <julian@mundobakia.com>
2018-10-16 15:34:22 -0500

    se corrigen errores y se agregan plugins

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,8 @@
 
     buscar.setAttribute('placeholder', 'Buscar recetas');
 
-
+    if (document.getElementById('newslater')) {
+        
     //campos registro de usuario
     let suscribite = document.querySelector('.newslater a'),
         datosUsuario = document.querySelector('#datos_usuario');
@@ -23,6 +24,7 @@
     email.addEventListener('blur', validarRegistro);
     email.addEventListener('blur', validarEmail);
     suscribite.addEventListener('click', registro);
+
 
     //Funciones
 /*
@@ -50,16 +52,12 @@
             alert('Este campo de llevar una @');
         }
     }
-    console.log('listo')
-
+}
 })();
 
 $(function (){
 
-//se agregan atributos al menu de redes dociales
-
-    $('div.contenido_footer .nosotros h2').text('Incentivate a realizar un cambio');
-    $('div.contenido_footer .nosotros p').text('Te alimentaras adecuadamente con estas ideas, daras más oportunidad de vida, y haras para ti una nueva oportunidad')
+    //se agregan atributos al menu de redes dociales
 
     $('.menu_redes_sociales ul li a:first').attr('href', 'https://www.facebook.com');
     $('.menu_redes_sociales ul li:first').next().children().attr('href', 'https://www.instagram.com/?hl=es-la');
@@ -68,25 +66,33 @@ $(function (){
     $('.sociales .menu_redes_sociales ul li:first').next().children().attr('href', 'https://www.instagram.com/?hl=es-la');
     $('.sociales .menu_redes_sociales ul li:first').next().next().children().attr('href', 'https://www.youtube.com');
 
+
 //newslatter
     $('div.newslater a').on('click', function(){
         $(this).hide();
-        $('#datos_usuario').show(1000);
-    })
+        $('#datos_usuario').show(800);
+        $('div.newslater p').animateNumber({number: 80}, 2000);
+
+    });
 
 //menu_principal
     $('nav.navegacion_principal ul li').on('click', function(){
-        $('nav.navegacion_principal ul li').removeClass('activo')
-       $(this).addClass('activo')
+        $('nav.navegacion_principal ul li').removeClass('activo');
+       $(this).addClass('activo');
     });
 
 //menu_movil
     $('.menu_movil').on('click', function(){
 
-        $('.navegacion_principal ul').slideDown(1000);
-    })
-    
-    console.log('correcto')
+        $('.navegacion_principal ul').slideToggle(1000);
 
 
-})
+    });
+
+// se cambia el contenido del footer
+
+$('div .nosotros h2').text('Incentivate a realizar un cambio');
+$('div .nosotros p').text('Te alimentaras adecuadamente con estas ideas, daras más oportunidad de vida, y haras para ti una nueva oportunidad')
+
+
+});
