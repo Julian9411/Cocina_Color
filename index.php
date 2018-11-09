ee22b9c4f368533b30d0b3544428919cb805b3eb
julian <julian@mundobakia.com>
2018-11-09 12:49:10 -0500

    se modifica index.php y la carpeta img

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
-
-
+<?php 
+if(isset($_GET['/'])){
+    $id = $_GET['/'];
+}
+try{
+    require_once('includes/funciones/bd_conexion.php');
+    $sql = "SELECT nombre_receta, nombre_receta, descripcion_receta,url_img_receta, id_categoria_receta, id_categorias_receta, id_categorias_receta FROM `recetas` ";
+    $sql .="INNER JOIN categoriasReceta ";
+    $sql .="ON recetas.id_categoria_receta = categoriasReceta.id_categorias_receta ";
+    $resultado = $conn->query($sql);
+}catch(Exception $e){
+    $error = $e->getMessage();
+}
+?>
 <!--Comiezo del Contenido principal del sitio-->
         <div class="contenido_principal clearfix">
 
@@ -11,7 +23,7 @@
             <div class="contenedor clearfix">
                     <h2>Top de recetas vegetarianas</h2>
                 <div class="receta_imagen">
-                    <img src="img/ALMUERZOS/1.jpg" alt="Comida_Vegetariana">
+                    <img src="img/almuerzo1.jpg" alt="Comida_Vegetariana">
                 </div><!--receta_imgagen-->
                  <div class="top_recetas">
 
@@ -20,56 +32,56 @@
                     <li>
                         <h2>NOODLES DE ARROZ</h2>
                         <div class="receta">
-                       <img src="img/ALMUERZOS/10.jpg" alt="Receta-vegetarina">
-                       <a href="almuerzos/recetas_almuerzos/receta10.html" class="boton invisible">Leer Mas</a>
+                       <img src="img/almuerzo10.jpg" alt="Receta-vegetarina">
+                       <a href="receta.php?id=27" class="boton invisible">Leer Mas</a>
                        </div>
                        <p>Estos fideos de arroz se usan mucho en la comida asíatica y se pueden preparar de muchas maneras, esta receta es rica y sumamente versátil. Con esta base puedes preparar de la forma y con las verduras que quieras.</p>
-                       <a href="almuerzos/recetas_almuerzos/receta10.html" class="boton">Leer Mas</a>
+                       <a href="receta.php?id=27" class="boton">Leer Mas</a>
                     </li>
                     <li>
                         <h2>TOSTADAS DE CHORIZO VEGANO</h2>
                         <div class="receta">
-                       <img src="img/CENAS/2.jpg" alt="Receta-vegetarina">
-                       <a href="cenas/recetas_cenas/receta2.html" class="boton invisible">Leer Mas</a>
+                       <img src="img/cenas2.jpg" alt="Receta-vegetarina">
+                       <a href="receta.php?id=6" class="boton invisible">Leer Mas</a>
                        </div>
                        <p>Este platillo es ideal para un lunes sin carne ya que no contiene nada de producto animal, pero está lleno de sabor. Ricas tostadas con frijoles y chorizo vegano a base de garbanzos y acompañados de una crema de aguacate. Anímate a prepáralas.</p>
-                       <a href="cenas/recetas_cenas/receta2.html" class="boton">Leer Mas</a>
+                       <a href="receta.php?id=6" class="boton">Leer Mas</a>
                     </li>
                     <li>
                         <h2>PAN FRANCÉS CLÁSICO</h2>
                         <div class="receta">
-                       <img src="img/DESAYUNOS/5.jpg" alt="Receta-vegetarina">
-                       <a href="desayunos/recetas_desayunos/receta5.html" class="boton invisible">Leer Mas</a>
+                       <img src="img/desayuno5.jpg" alt="Receta-vegetarina">
+                       <a href="receta.php?id=12" class="boton invisible">Leer Mas</a>
                        </div>
                        <p>Una gran receta para preparar el Pan Francés, también conocido como French Toast. Se acompañan de miel de maple, mantequilla y se les puede poner fruta encima, como fresas.</p>
-                       <a href="desayunos/recetas_desayunos/receta5.html" class="boton">Leer Mas</a>
+                       <a href="receta.php?id=12" class="boton">Leer Mas</a>
                     </li>
                     <li>
                         <h2>PUDDING DE CHÍA Y MANGO</h2>
                         <div class="receta">
-                       <img src="img/POSTRES/1.jpg" alt="Receta-vegetarina">
-                       <a href="postres/recetas_postres/receta1.html" class="boton invisible">Leer Mas</a>
+                       <img src="img/postres1.jpg" alt="Receta-vegetarina">
+                       <a href="receta.php?id=7" class="boton invisible">Leer Mas</a>
                        </div>
                        <p>Postre crudivegano, que además de tener un sabor delicioso, es nutritivo y fresco. Lo puedes acompañar con la fruta de tu elección y de esta manera darle un toque de sabor personalizado.</p>
-                       <a href="postres/recetas_postres/receta1.html" class="boton">Leer Mas</a>
+                       <a href="receta.php?id=7" class="boton">Leer Mas</a>
                     </li>
                         <li>
                             <h2>PUDÍN DE PLÁTANO LIGHT</h2>
                             <div class="receta">
-                            <img src="img/POSTRES/5.jpg" alt="Receta-vegetarina">
-                             <a href="postres/recetas_postres/receta5.html" class="boton invisible">Leer Mas</a>
+                            <img src="img/postres5.jpg" alt="Receta-vegetarina">
+                             <a href="receta.php?id=38" class="boton invisible">Leer Mas</a>
                             </div>
                             <p>Esta rica receta de pudín de plátano es una versión del favorito original pero con menos calorias gracias a la leche reducida en grasa (leche light) y las claras de huevo.</p>
-                            <a href="postres/recetas_postres/receta5.html" class="boton">Leer Mas</a>
+                            <a href="receta.php?id=38" class="boton">Leer Mas</a>
                         </li>
                     <li>
                         <h2>FLAN NAPOLITANO TRADICIONAL</h2>
                         <div class="receta">
-                       <img src="img/POSTRES/9.jpg" alt="Receta-vegetarina">
-                       <a href="postres/recetas_postres/receta9.html" class="boton invisible">Leer Mas</a>
+                       <img src="img/postres9.jpg" alt="Receta-vegetarina">
+                       <a href="receta.php?id=42" class="boton invisible">Leer Mas</a>
                        </div>
                        <p>Esta receta de Flan Napolitano tradicional no puede faltar en tu recetario de repostería.</p>
-                       <a href="postres/recetas_postres/receta9.html" class="boton">Leer Mas</a>
+                       <a href="receta.php?id=42" class="boton">Leer Mas</a>
                     </li>                    
                 </ol>
                      </div><!--top_recetas-->
@@ -81,28 +93,28 @@
                         <h2>Nuestro Menú de recetas</h2>
                         <ul>
                             <li>
-                                <img src="img/DESAYUNOS/2.jpg" alt="Pastas vegetarianas">
+                                <img src="img/desayunos2.jpg" alt="Pastas vegetarianas">
                                 <div class=" clearfix" >
                                     <p>Recetas de desayunos rapidas y faciles de preparar como panquques, chilaquiles, omelette y muchos mas</p>
                                     <a href="desayunos/desayunos.html">Ver recetas de desayunos Vegetarianas</a>
                                 </div>
                             </li>
                              <li>
-                                <img src="img/ALMUERZOS/7.jpg" alt="Pastas vegetarianas">
+                                <img src="img/almuerzo7.jpg" alt="Pastas vegetarianas">
                                 <div class="contenedor clearfix" >
                                     <p>Prepara deliciosas Pastas Vegetarianas,llenas de nutrientes e ingrendiente accesibles.</p>
                                     <a href="almuerzos/almuerzos.html">Ver recetas de Pastas Vegetarianas</a>
                                  </div>
                             </li>
                              <li>
-                                <img src="img/ALMUERZOS/5.jpg" alt="Pastas vegetarianas">
+                                <img src="img/almuerzo5.jpg" alt="Pastas vegetarianas">
                                 <div class="contenedor clearfix" >
                                     <p>Deliciosos platos fuertes vegetarianos muy nutritivos que te dejaran satisfecho y son faciles de preparar</p>
                                     <a href="almuerzos/almuerzos.html">Ver recetas de platos fuertes Vegetarianas</a>
                                  </div>
                             </li>
                              <li>
-                                <img src="img/CENAS/5.jpg" alt="Pastas vegetarianas">
+                                <img src="img/cenas5.jpg" alt="Pastas vegetarianas">
                                 <div class="contenedor clearfix" >
                                     <p>Platillos vegetarianas con los nutrientes de la soya deliciosos para cualquier ocasion</p>
                                     <a href="cenas/cenas.html">Ver recetas de soya Vegetarianas</a>
@@ -114,88 +126,34 @@
 <!--Comienzo de Articulo recetas-->           
                 <article id="recetas" class="clearfix">
                         <div class="contenedor recetas clearfix">
+                            <?php
+                                try{
+                                require_once('includes/funciones/bd_conexion.php');
+                                $sql = "SELECT  id_receta, nombre_receta, descripcion_receta,url_img_receta, id_categoria_receta, id_categorias_receta, id_categorias_receta FROM `recetas` ";
+                                $sql .="INNER JOIN categoriasReceta ";
+                                $sql .="ON recetas.id_categoria_receta = categoriasReceta.id_categorias_receta ";
+                                $sql .="ORDER BY RAND() LIMIT 10";
+                                $resultado = $conn->query($sql);
+                            }catch(Exception $e){
+                                $error = $e->getMessage();
+                            }
+                            ?>
                             <h2>Recetas</h2>
                             <ul>
+                                <?php 
+                                while($receta = $resultado->fetch_assoc()){
+                                ?>
                                 <li>
-                                    <a href="cenas/recetas_cenas/receta2.html">
-                                    <img src="img/CENAS/2.jpg" alt="Foto de recetas">
-                                    <h3>TOSTADAS DE CHORIZO VEGANO</h3>
-                                    <p>Este platillo es ideal para un lunes sin carne ya que no contiene nada de producto animal... </span></p>
+                                    <a href="receta.php?id=<?php echo $receta['id_receta'] ?>">
+                                    <img src="img/<?php echo $receta['url_img_receta'] ?>" alt="Foto de <?php echo $receta['nombre_receta'] ?>">
+                                    <h3><?php echo $receta['nombre_receta'] ?></h3>
+                                    <p><?php echo $receta['descripcion_receta'] ?></p>
                                     </a>
                                 </li>
-                                <li>
-                                    <a href="cenas/recetas_cenas/receta4.html">
-                                    <img src="img/CENAS/4.jpg" alt="Foto de recetas">
-                                    <h3>PASTA ALFREDO VEGANA</h3>
-                                    <p>Anímate a probar esta deliciosa pasta alfredo vegana que en tan sólo 3 pasos...</span></p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="postres/recetas_postres/receta4.html">
-                                    <img src="img/POSTRES/4.jpg" alt="Foto de recetas">
-                                    <h3>PANQUÉ DE CANELA, CLAVO Y MANZANAS</h3>
-                                    <p>Pan sencillo y nutritivo para tener desayunos energéticos que además de mucho beneficios... </span></p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="postres/recetas_postres/receta7.html">
-                                    <img src="img/POSTRES/7.jpg" alt="Foto de recetas">
-                                    <h3>PASTEL DE MAMEY</h3>
-                                    <p>Delicioso pastel con un sabor muy original y delicioso a mamey. Es muy recomendable para...</p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="cenas/recetas_cenas/receta8.html">
-                                    <img src="img/CENAS/8.jpg" alt="Foto de recetas">
-                                    <h3>AGUACATES RELLENOS DE ENSALADA MEDITERRÁNEA</h3>
-                                    <p>Esta receta de ensalada de aguacate es única e inigualable. Deliciosos aguacates...</span></p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="desayunos/recetas_desayunos/receta4.html">
-                                    <img src="img/DESAYUNOS/4.jpg" alt="Foto de recetas">
-                                    <h3>HUARACHE DE NOPAL CON QUESO PANELA</h3>
-                                    <p>Esta receta es una deliciosa opción alternativa ya que es baja en calorías y alta en fibra... </span></p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="desayunos/recetas_desayunos/receta9.html">
-                                    <img src="img/DESAYUNOS/9.jpg" alt="Foto de recetas">
-                                    <h3>SÁNDWICH DE AGUACATE, QUESO DE CABRA</h3>
-                                    <p>sta rica y rápida opción para preparar un sándwich, empieza tu día con toda la energía... </span></p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="postres/recetas_postres/receta8.html">
-                                    <img src="img/POSTRES/8.jpg" alt="Foto de recetas">
-                                    <h3>PAY DE LIMÓN CON MERENGUE FRANCÉS</h3>
-                                    <p>Esta delicioso pay de limón es increíble con una masa crujiente, un exquisito relleno de... </span></p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="bebidas/recetas_bebidas/receta4.html">
-                                    <img src="img/BEBIDAS/4.jpg" alt="Foto de recetas">
-                                    <h3>SMOOTHIE DE TÉ VERDE CON PLÁTANO</h3>
-                                    <p>Este batido llenará tu día de energía, además de nutritivo es muy refrescante y depurativo... </span></p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="desayunos/recetas_desayunos/receta6.html">
-                                    <img src="img/DESAYUNOS/6.jpg" alt="Foto de recetas">
-                                    <h3>NQUINOA CON LECHE DE ALMENDRAS Y VAINILLA</h3>
-                                    <p>Empieza tu día con toda la energía con este delicioso y nutritivo bowl de quinoa con leche... </span></p>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                    <img src="img/CENAS/2.jpg" alt="Foto de recetas">
-                                    <h3>TABULE DE LENTEJAS</h3>
-                                    <p>Este platillo de tabule inspirado en la cocina libanesa, además de su exquisito sabor, te ... </span></p>
-                                    </a>
-                                </li>
-                                
+                                <?php } ?> 
                             </ul>
                         </div>
+
                 </article>
                 <article id="newslater">
                     <div class="contenedor newslater ">
